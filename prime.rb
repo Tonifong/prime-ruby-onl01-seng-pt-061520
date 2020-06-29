c288396c68103be1e8cfcd256a3dc9c2337e13fc
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-29 22:10:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(value)
+    #prime num is greater than one and
+    #not a product of 2 smaller nums
+    if value <= 1 || value == 0 || value == 1
+      return false
+    elsif
+      (2..value - 1).each do |i|
+        if value % i == 0
+          return false
+        end
+      end
+    end
+    true
+  end
